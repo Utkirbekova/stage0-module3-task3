69b5d88396cf25755f5c1fc4695726838638513b
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-06 22:36:57 +0300

    .

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -7,9 +7,9 @@ public class ReassigningValues {
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);
-        System.out.println("linkToFirst" + 15);
-        System.out.println("linkToSecond" + 6);
-        System.out.println("linkToThird" + 4);
+        System.out.println(15);
+        System.out.println(6);
+        System.out.println(4);
         System.out.println(first);
         System.out.println(second);
         System.out.println(third);

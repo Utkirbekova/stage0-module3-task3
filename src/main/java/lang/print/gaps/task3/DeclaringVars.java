e6c4d3fca993c09cf4b1ad77ff2c9e0da40cf42e
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-06 22:33:33 +0300

    .

--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -2,6 +2,7 @@ package lang.print.gaps.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        //write code here
+        int n = 10;
+        System.out.println(n);
     }
 }
--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,17 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+
+        int first = 1, second = 10, third = 100;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println("linkToFirst" + 15);
+        System.out.println("linkToSecond" + 6);
+        System.out.println("linkToThird" + 4);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
     }
 }

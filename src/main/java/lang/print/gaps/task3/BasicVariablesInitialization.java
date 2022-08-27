3a9913a8b30e459b643a08cc885b81885749d0b2
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-27 20:23:24 +0300

    .

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -2,7 +2,6 @@ package lang.print.gaps.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
         int first = 1;
         int second = 10;
         int third = 100;
